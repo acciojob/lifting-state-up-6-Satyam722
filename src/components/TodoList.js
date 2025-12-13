97d719cef550571d9613494efe25d3339764c768
Satyam <nikamsatyam187@gmail.com>
2025-12-13 20:11:04 +0530

    Update TodoList.js

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,23 +1,34 @@
 import React from "react";
 
-const TodoList = ({ todos, handleComplete }) => {
-    return (
-        <div className="child-component">
-            <h2>Child Component</h2>
-            <ul>
-                {todos.map(todo => (
-                    <li key={todo.id}>
-                        {todo.text}
-                        {!todo.completed && (
-                            <button onClick={() => handleComplete(todo.id)}>
-                                Complete
-                            </button>
-                        )}
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
-};
+function TodoList({ todos, handleComplete }) {
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <li key={todo.id} style={{ marginBottom: "10px" }}>
+          <span
+            style={{
+              textDecoration: todo.completed ? "line-through" : "none",
+              marginRight: "10px",
+            }}
+          >
+            {todo.task}
+          </span>
+
+          {!todo.completed && (
+            <button onClick={() => handleComplete(todo.id)}>
+              Complete
+            </button>
+          )}
+
+          {todo.completed && (
+            <span style={{ color: "green", fontWeight: "bold" }}>
+              ✔ Completed
+            </span>
+          )}
+        </li>
+      ))}
+    </ul>
+  );
+}
 
 export default TodoList;
